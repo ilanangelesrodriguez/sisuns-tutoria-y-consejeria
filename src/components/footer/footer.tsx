3596a4e75d1d9fbf58c8d5a2afcac35fa712070e
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2024-10-10 01:11:48 +0000

    Agregación del Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,16 +1,49 @@
-import { Link } from "@nextui-org/link";
+import './style_footer.css';
 
 const Footer = () => {
   return (
-    <footer className="w-full flex items-center justify-center py-3">
-      <Link
-        className="flex items-center gap-1 text-current"
-        href="/"
-        title="inicio"
-      >
-        <span className="text-default-600">Desarrollado por </span>
-        <p className="text-primary">SISUNS</p>
-      </Link>
+    <footer className="footer">
+      {/* Barra de navegación */}
+      <nav className="footer-nav">
+        <div className="container_item">
+          <a href="#">Inicio</a>
+          <a href="#">Blog</a>
+          <a href="#">Nosotros</a>
+          <a href="#">Contactos</a>
+          <a href="#">Reglamento</a>
+          <a href="#">Políticas de Privacidad</a>
+        </div>
+        <div className="container_item">
+          <span>Encuentranos en Av. Pacífico 508 - Nuevo Chimbote</span>
+        </div>
+
+        <div className="container_item container_item_logo">
+        <img
+            src="/images/footer/logo_episi.png"
+            alt="Universidad Nacional del Santa"
+            className="imagen_universidad"
+          />
+          <img
+            src="/images/footer/logo_rojo.png"
+            alt="Universidad Nacional del Santa"
+            className="imagen_universidad"
+          />
+        </div>
+      </nav>
+
+      {/* Sección de copyright y redes sociales */}
+      <div className="footer-bottom">
+        <div className="container">
+          <p className="copyright">
+            Diseñado por <span className="bold">SISUNS</span> | Copyright © 2024
+          </p>
+          <div className="social-icons">
+            <a href="https://www.facebook.com/UNSChimbote/?locale=es_LA" className="icon"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.youtube.com/channel/UCcCyx2mzVyJrktsFs9IBdGQ" className="icon"><i className="fab fa-youtube"></i></a>
+            <a href="https://www.instagram.com/unschimbote/?hl=es" className="icon"><i className="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
