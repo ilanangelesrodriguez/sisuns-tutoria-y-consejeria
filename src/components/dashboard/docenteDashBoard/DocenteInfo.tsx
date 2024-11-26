ae0bd0a37b829b833f54193d9c9d1893ee77ba3e
jhoancc21 <116601508+Jhoan2-cpu@users.noreply.github.com>
2024-11-25 23:52:04 -0500

    generación

--- src/components/dashboard/docenteDashBoard/DocenteInfo.tsx
+++ src/components/dashboard/docenteDashBoard/DocenteInfo.tsx
@@ -1,14 +1,7 @@
 import { Card } from "@nextui-org/react";
 import Styles from "@/components/dashboard/docenteDashBoard/DocenteDashboard.module.css";
-import "./DocenteDashboard.module.css";
-// #92B7AD ...gris______ color-92B7AD
-// #C7D4D8 ...gris claro gaa______ color-C7D4D8
-// #ABBFC6 ...gris metalico______ color-ABBFC6
-// #41B3A4 … verde fuerte______ color-41B3A4
-// #50AB91 … verde sucio______ color-50AB91
 
 const DocenteInfo = () => (
-//className="bg-white text-black dark:bg-gray-800 dark:text-white p-4 rounded"
   <Card className="dark:bg-wo-41B3A4 dark:text-white w-full text-center p-4 rounded-md border" shadow='none'>
     <h3 className={Styles.title}>INFORMACIÓN DEL DOCENTE</h3>
     <p className="text-foreground mt-2">
