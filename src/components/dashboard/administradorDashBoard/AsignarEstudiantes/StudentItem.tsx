769b8fe7d10f73d2f0bdfcb588c05afd96531cee
jhoancc21 <116601508+Jhoan2-cpu@users.noreply.github.com>
2024-12-01 20:16:27 -0500

    v2 change

--- src/components/dashboard/administradorDashBoard/AsignarEstudiantes/StudentItem.tsx
+++ src/components/dashboard/administradorDashBoard/AsignarEstudiantes/StudentItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, ChipProps, TableCell, User, Button, Chip, Tooltip } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip } from "@nextui-org/react";
 import { columns } from "./data"; // Asegúrate de que columns tenga claves correctas
 import Estudiante from "@/models/estudiante";
 import { EditIcon, EyeIcon } from "@/components/icons";
